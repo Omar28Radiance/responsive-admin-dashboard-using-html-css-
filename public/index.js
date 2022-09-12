1c636de6295f7fa8ddf0809118712ce54aef44d0
Omar28Radiance <omarin0a@hotmail.com>
2022-09-12 11:28:07 -0500

    index.js menuBtn.addEventListener

--- public/index.js
+++ public/index.js
@@ -1,3 +1,7 @@
+const { click } = require("@testing-library/user-event/dist/click");
+
 const sideMenu = document.querySelector("aside");
 const menuBtn = document.querySelector("#menu-btn");
 const closeBtn = document.querySelector("#close-btn");
+
+menuBtn.addEventListener('click', {} => {});
