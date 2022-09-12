c20540605712598fdead84b045a0b4e0b1ba2bed
Omar28Radiance <omarin0a@hotmail.com>
2022-09-12 13:11:17 -0500

    js const themeToggler = document.querySelector

--- public/index.js
+++ public/index.js
@@ -1,11 +1,14 @@
 const sideMenu = document.querySelector("aside");
 const menuBtn = document.querySelector("#menu-btn");
 const closeBtn = document.querySelector("#close-btn");
+const themeToggler = document.querySelector(".themeToggler");
 
+// show sidebar
 menuBtn.addEventListener('click', () => {
     sideMenu.style.display = 'block';
 })
 
+// close sidebar
 closeBtn.addEventListener('click', () => {
     sideMenu.style.display = 'none';
 })
