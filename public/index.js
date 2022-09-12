9651b419d1492f1e4a7022723258f2dd8bb17d62
Omar28Radiance <omarin0a@hotmail.com>
2022-09-12 17:24:58 -0500

    js fill orders in table

--- public/index.js
+++ public/index.js
@@ -26,12 +26,12 @@ themeToggler.addEventListener('click', () => {
 Orders.forEach(order => {
     const tr = document.createElement('tr');
     const trContent = `
-        <td>${order.productName}</td>
-        <td>${order.productNumber}</td>
-        <td>${order.paymentStatus}</td>
-        <td class="${order.shipping === 'Declined' ? 'danger' : order.shipping === 'pending' ? 'warning' : 'primary'}">${order.shipping}</td>
-        <td class="primary">Details</td>
-                        `;
+                        <td>${order.productName}</td>
+                        <td>${order.productNumber}</td>
+                        <td>${order.paymentStatus}</td>
+                        <td class="${order.shipping === 'Declined' ? 'danger' : order.shipping === 'pending' ? 'warning' : 'primary'}">${order.shipping}</td>
+                        <td class="primary">Details</td> 
+                        `
     tr.innerHTML = trContent
     document.querySelector('table tbody').appendChild(tr);
 })
